331abd018d24141f8cb3f07a6444e5e74265a31d
Vipsoni689 <109866709+Vipsoni689@users.noreply.github.com>
2022-08-02 21:40:39 +0530

    Update js.js

--- js.js
+++ js.js
@@ -6,6 +6,21 @@ $('header').toggleClass('toggle');
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-time');
         $('header').removeClass('toggle');
+        if($(window).scrollTop()>0){
+            $('.top').show();
+        }else{
+            $('.top').show();
+        }
 
     });
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop:$($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+    });
+
 });
